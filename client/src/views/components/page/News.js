aef1e266589a8cd872630fea4d5fbb2de5d00085
bui thanh <tieungpk1@gmail.com>
2022-05-07 00:29:28 +0700

    update

--- client/src/views/components/page/News.js
+++ client/src/views/components/page/News.js
@@ -3,49 +3,62 @@ import { Container, Row, Col, Card } from "react-bootstrap";
 import CardLH from "../../../components/News/cardlh";
 import CardInfor from "../../../components/News/cardinfor";
 import CardLeft from "../../../components/News/cardleft";
-import PleaseBuyACar from '../content/home/PleaseBuyACar';
+import PleaseBuyACar from "../content/home/PleaseBuyACar";
 import { useContext, useState } from "react";
-import PaginationCus from '../../../components/pagination/PaginationCus';
+import PaginationCus from "../../../components/pagination/PaginationCus";
 import { ProductContext } from "../../../contexts/ProductContext";
 
 export default function News() {
   const {
     productState: { products },
   } = useContext(ProductContext);
-  const productcategory = products.filter(item=>item.category==='logo')
-
+  const productcategory = products.filter((item) => item.category === "logo");
 
   const [currentPage, setCurrentPage] = useState(1);
-    const [productsPerPage] = useState(8);
-    // Get Current Products
-    const indexOfLastProduct = currentPage * productsPerPage ;
-    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-    const currentProducts = productcategory.slice(indexOfFirstProduct,indexOfLastProduct);
-    // ChangePage
-    const paginate = pageNumber => {
-        setCurrentPage(pageNumber);
-    }
+  const [productsPerPage] = useState(8);
+  // Get Current Products
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = productcategory.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+  // ChangePage
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
   return (
     <div style={{ padding: "36px 0" }}>
-      
-                <div  className="TitleCar d-flex">
-                    <div style={{textAlign:'center',textTransform:'uppercase',fontSize:'19px',margin:'auto',fontWeight:'bold'}}>
-                        <div className="SubTitleCar"> </div>
-                        <hr style={{borderBottom:'4px solid',width:'75%',color:'rgb(255, 11, 11)'}}/>
-                    </div>
-                </div>
-                <PleaseBuyACar products={productcategory} />
-     
-        <div className="d-flex justify-content-center pt-5" >
-            <PaginationCus
-                productsPerPage={productsPerPage}
-                totalProducts={productcategory.length}
-                paginate={paginate}
-                currentPage={currentPage}
-            />
+      <div className="TitleCar d-flex">
+        <div
+          style={{
+            textAlign: "center",
+            textTransform: "uppercase",
+            fontSize: "19px",
+            margin: "auto",
+            fontWeight: "bold",
+          }}
+        >
+          <div className="SubTitleCar"> </div>
+          <hr
+            style={{
+              borderBottom: "4px solid",
+              width: "75%",
+              color: "rgb(255, 11, 11)",
+            }}
+          />
         </div>
- 
+      </div>
+      <PleaseBuyACar products={productcategory} />
+
+      <div className="d-flex justify-content-center pt-5">
+        <PaginationCus
+          productsPerPage={productsPerPage}
+          totalProducts={productcategory.length}
+          paginate={paginate}
+          currentPage={currentPage}
+        />
+      </div>
     </div>
-    
   );
 }
--- client/src/views/components/page/Products.js
+++ client/src/views/components/page/Products.js
@@ -1,42 +1,43 @@
 import React from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
-import PleaseBuyACar from '../content/home/PleaseBuyACar';
+import PleaseBuyACar from "../content/home/PleaseBuyACar";
 import { useContext, useState } from "react";
-import PaginationCus from '../../../components/pagination/PaginationCus';
+import PaginationCus from "../../../components/pagination/PaginationCus";
 import { ProductContext } from "../../../contexts/ProductContext";
 
 export default function Products() {
-    const {
-        productState: { products },
-    } = useContext(ProductContext);
-    const productcategory = products.filter(item => item.category === 'graphics')
-    const [currentPage, setCurrentPage] = useState(1);
-    const [productsPerPage] = useState(8);
-    // Get Current Products
-    const indexOfLastProduct = currentPage * productsPerPage;
-    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-    const currentProducts = productcategory.slice(indexOfFirstProduct, indexOfLastProduct);
-    // ChangePage
-    const paginate = pageNumber => {
-        setCurrentPage(pageNumber);
-    }
-    return (
-        <Container style={{ padding: "36px 0" }}>
-            <Container style={{ padding: '36px 0' }}>
-                <h1 style={{color:'black'}}></h1>
-                <PleaseBuyACar products={productcategory} />
-            </Container>
+  const {
+    productState: { products },
+  } = useContext(ProductContext);
+  const productcategory = products.filter(
+    (item) => item.category === "graphics"
+  );
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage] = useState(8);
+  // Get Current Products
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = productcategory.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+  // ChangePage
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+  return (
+    <div style={{ padding: "36px 0" }}>
+      <h1 style={{ color: "black" }}></h1>
+      <PleaseBuyACar products={productcategory} />
 
-            <div className="d-flex justify-content-center pt-5" >
-                <PaginationCus
-                    productsPerPage={productsPerPage}
-                    totalProducts={productcategory.length}
-                    paginate={paginate}
-                    currentPage={currentPage}
-                />
-            </div>
-
-        </Container>
-
-    );
+      <div className="d-flex justify-content-center pt-5">
+        <PaginationCus
+          productsPerPage={productsPerPage}
+          totalProducts={productcategory.length}
+          paginate={paginate}
+          currentPage={currentPage}
+        />
+      </div>
+    </div>
+  );
 }
